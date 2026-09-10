240638b1f24729af6dc313cbd8292ff89bd2acff
Nayan Sorthi <nds15@student.le.ac.uk>
2026-09-10 12:31:46 +0100

    Stage 1: Leaflet map rendering, centered on Leicester

---
+++ js/map.js
@@ -0,0 +1,7 @@
+const map = L.map("map").setView([52.6369, -1.1398], 13);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
